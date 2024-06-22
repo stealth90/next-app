95a5a0023f1f7ecc6c12797762c904698257f621
stealth90 <pierantonio.petralia@live.it>
2024-06-22 18:40:37 +0200

    feat: build api for create issue

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,13 +6,14 @@ import React from 'react';
 import { AiFillBug } from 'react-icons/ai';
 import classNames from 'classnames';
 
+const links = [
+  { label: 'Dashboard', href: '/' },
+  { label: 'Issues', href: '/issues' },
+];
+
 const NavBar = () => {
   const currentPath = usePathname();
-  console.log({ currentPath });
-  const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues' },
-  ];
+
   return (
     <div className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
